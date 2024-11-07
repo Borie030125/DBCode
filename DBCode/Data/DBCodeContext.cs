ec981285e3bfe88b7de7fa622980a01704dd2f48
Xuan Wang <wang6xu@mail.uc.edu>
2024-11-08 02:44:43 +0800

    添加项目文件。

---
+++ DBCode/Data/DBCodeContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DBCode.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBCode.Data
+{
+
+    public class DBCodeContext : DbContext
+    {
+        public DBCodeContext (DbContextOptions<DBCodeContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().HasData(
+                new Student { Id = 1, Name = "Todd", City = "Cincinnati", Age = 20 },
+                new Student { Id = 2, Name = "Bob", City = "Cincinnati", Age = 19 }
+                );
+        }
+
+        public DbSet<DBCode.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ DBCode/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace DBCode.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public int Age { get; set; }
+        public Student()
+        {
+
+        }
+
+        public Student(int id, string name, string city, int age)
+        {
+            Id = id;
+            Name = name;
+            City = city;
+            Age = age;
+        }
+    }
+}
